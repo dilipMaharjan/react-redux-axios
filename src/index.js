7de218f0de22b4f889677d2351b344174aab64c2
dmaharjan <dilipmaharjann@gmail.com>
2017-09-02 20:41:25 -0400

    working with redux

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import {createStore} from 'redux';
+const reducer=function(state,action){
+switch(action.type){
+  case "INC":
+  return state+action.payload;
+  case "DEC":
+    return state-action.payload;
+  default:
+  return state;
+}
+}
+
+const store =createStore(reducer,0);
+store.subscribe(()=>{
+  console.log("Store changed",store.getState());
+});
+store.dispatch({type:"INC",payload:1});
+store.dispatch({type:"INC",payload:1});
+store.dispatch({type:"INC",payload:1});
+store.dispatch({type:"INC",payload:1});
+store.dispatch({type:"DEC",payload:1});
