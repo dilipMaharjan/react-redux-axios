e8aa947238917451f28f03760712be7927b01f83
dmaharjan <dilipmaharjann@gmail.com>
2017-09-04 20:00:04 -0400

    mutating state, not good practice

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
-import {combineReducer,createStore} from 'redux';
-const reducer=function(state,action){
-switch(action.type){
-  case "INC":
-  return state+action.payload;
-  case "DEC":
-    return state-action.payload;
-  default:
-  return state;
-}
-}
+import {combineReducers,createStore} from 'redux';
+const userReducer=(state={},action)=>{
+  switch(action.type){
+case "NAME":
+state.name=action.payload;
+break;
+case "AGE":
+state.age=action.payload;
+break;
+default:
+return state;
+  }
+return state;
+};
+const tweetsReducer=(state=[],action)=>{
+//state=action.payload;
+return state;
+};
+const reducer=combineReducers({
+user:userReducer,
+tweets:tweetsReducer
+});
 
-const store =createStore(reducer,0);
+const store =createStore(reducer);
 store.subscribe(()=>{
   console.log("Store changed",store.getState());
 });
-store.dispatch({type:"INC",payload:1});
-store.dispatch({type:"INC",payload:1});
-store.dispatch({type:"INC",payload:1});
-store.dispatch({type:"INC",payload:1});
-store.dispatch({type:"DEC",payload:1});
+store.dispatch({type:"NAME",payload:"Will"});
+store.dispatch({type:"AGE",payload:35});
