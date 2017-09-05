f679d99ee9f4290631b4a78933d09ea314bd3eed
dmaharjan <dilipmaharjann@gmail.com>
2017-09-04 22:01:31 -0400

    asynchronous call using thunk and axios

--- src/index.js
+++ src/index.js
@@ -1,34 +1,38 @@
-import {applyMiddleware,combineReducers,createStore} from 'redux';
+import {applyMiddleware,createStore} from 'redux';
 import logger from 'redux-logger';
-const userReducer=(state={},action)=>{
+import axios from 'axios';
+import thunk from 'redux-thunk';
+const initialState={
+  fetching:false,
+  fetched:false,
+  users:[],
+  error:null,
+};
+const reducer=(state=initialState,action)=>{
   switch(action.type){
-case "NAME":
-state={...state,name:action.payload};
-break;
-case "AGE":
-state={...state,age:action.payload};
-break;
-default:
-return state;
+case "FETCH_USER_START":
+return ({...state,fetching:true});
+case "FETCH_USERS_ERROR":
+return ({...state,fetching:false,error:action.payload});
+case "RECEIVE_USERS":
+return ({...state,fetching:false,fetched:true,users:action.payload});
   }
 return state;
 };
-const tweetsReducer=(state=[],action)=>{
-return state;
-};
-// const logger=(store)=>(next)=>(action)=>{
-//   console.log("Action fired",action);
-//   next(action);
-// };
-const middleware=applyMiddleware(logger);
-const reducer=combineReducers({
-user:userReducer,
-tweets:tweetsReducer,
-});
 
+const middleware=applyMiddleware(thunk,logger);
 const store =createStore(reducer,middleware);
 store.subscribe(()=>{
   console.log("Store changed",store.getState());
 });
-store.dispatch({type:"NAME",payload:"Will"});
-store.dispatch({type:"AGE",payload:35});
+store.dispatch((dispatch)=>{
+  dispatch({type:"FETCH_START_USER"});
+  axios.get("http://rest.learncode.academy/api/wstern/users")
+  .then((response)=>{
+    dispatch({type:"RECEIVE_USERS",payload:response.data})
+  })
+    .catch((error)=>{
+      dispatch({type:"FETCH_USERS_ERROR",payload:error})
+
+  })
+});
