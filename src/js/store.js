988e0fe7e2290b6226ce3a04e258263791d5a3af
dilipMaharjan <dilipmaharjann@gmail.com>
2017-09-06 14:28:05 -0400

    connecting redux to browswer

--- src/js/store.js
+++ src/js/store.js
@@ -2,6 +2,6 @@ import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import reducer from "./reducers";
-
-const middleware = applyMiddleware(thunk, logger);
+import { composeWithDevTools } from "redux-devtools-extension";
+const middleware = composeWithDevTools(applyMiddleware(thunk, logger));
 export default createStore(reducer, middleware);
